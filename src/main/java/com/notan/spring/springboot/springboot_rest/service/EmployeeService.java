1dfedcfdd7c54dc47b479c394fb4126ea9bbb622
Anton Moskovchenko <moskovanton@gmail.com>
2021-01-10 17:59:49 +0300

    getAllEmployees method added

---
+++ src/main/java/com/notan/spring/springboot/springboot_rest/service/EmployeeService.java
@@ -0,0 +1,19 @@
+package com.notan.spring.springboot.springboot_rest.service;
+
+
+
+import com.notan.spring.springboot.springboot_rest.entity.Employee;
+
+import java.util.List;
+
+public interface EmployeeService {
+
+    public List<Employee> getAllEmployees();
+
+//    public void saveEmployee(Employee employee);
+//
+//    public Employee getEmployee(int id);
+//
+//    public void deleteEmployee(int id);
+
+}
